cfc01fd9221e07507651c0dc76a3e9e6d17ddd28
Reda452 <redasakhi0@gmail.com>
2025-03-15 09:49:36 +0000

    New

---
+++ activity1.py
@@ -0,0 +1,18 @@
+#class creation
+class myClass:
+
+    # private variable
+    __privatevar = 27
+
+    # private method
+    def privMeth(self):
+        print("I'm inside class myClass")
+
+    # Function to print value of private variable
+    def hello(self):
+        print("Private Variable value: ",myClass.__privatevar)
+
+# object creation and method call
+foo = myClass()
+foo.hello()
+foo.privMeth()
